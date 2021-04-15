90bf157abb8218e8abe96c252e3952baeddff213
bapplewh <bapps.5@gmail.com>
2021-04-14 21:59:23 -0600

    created recent posts + searchbar components, and imported them into app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Logo from "./logo";
+import SearchBar from "./search-bar";
+import RecentPosts from "./recent-posts";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <Logo />
+        <SearchBar />
+        <RecentPosts />
       </div>
     );
   }
